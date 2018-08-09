e7714842b39c98bcdb7bb414aa8aff8da5fd3d6a
Yi Xu <yi@lavamap.com>
2018-08-09 18:13:51 -0400

    upate popup

--- main.js
+++ main.js
@@ -1,168 +1,3 @@
-var layerStyle = {
-	ridership_16: {
-		color: [
-			'interpolate',
-	    	['linear'],
-	    	['get', 'wkday_16'],
-	    	300,
-	    	'#ff6e6e',
-	    	20000,
-	    	'#ff5252',
-	    	70000,
-	    	'#ff2a2a'
-		],
-		radius: [
-			'interpolate',
-		  	['exponential', 1.75],
-		  	['zoom'],
-		  	11,
-		  	[
-		    	'interpolate',
-		    	['linear'],
-		    	['get', 'wkday_16'],
-		    	300,
-		    	1,
-		    	20000,
-		    	4,
-		    	70000,
-		    	7
-		  	],
-		  	14,
-		  	[
-		    	'interpolate',
-		    	['linear'],
-		    	['get', 'wkday_16'],
-		    	300,
-		    	10,
-		    	20000,
-		    	15,
-		    	70000,
-		    	30
-		  	],
-		  	16,
-		  	[
-		    	'interpolate',
-		    	['linear'],
-		    	['get', 'wkday_16'],
-		    	300,
-		    	20,
-		    	20000,
-		    	30,
-		    	70000,
-		    	70
-		  	]
-		]
-	},
-	ridership_growth_1yr: {
-		color: [
-			'interpolate',
-	    	['linear'],
-	    	['get', 'wkday_1yr_'],
-	    	-0.75,
-	    	'#ff6e6e',
-	    	0,
-	    	'#ff5252',
-	    	0.1,
-	    	'#ff2a2a'
-		],
-		radius: [
-			'interpolate',
-		  	['exponential', 1.75],
-		  	['zoom'],
-		  	11,
-		  	[
-		    	'interpolate',
-		    	['linear'],
-		    	['get', 'wkday_16'],
-		    	-0.75,
-		    	1,
-		    	0,
-		    	4,
-		    	0.1,
-		    	7
-		  	],
-		  	14,
-		  	[
-		    	'interpolate',
-		    	['linear'],
-		    	['get', 'wkday_16'],
-		    	-0.75,
-		    	10,
-		    	0,
-		    	15,
-		    	0.1,
-		    	30
-		  	],
-		  	16,
-		  	[
-		    	'interpolate',
-		    	['linear'],
-		    	['get', 'wkday_16'],
-		    	-0.75,
-		    	20,
-		    	0,
-		    	30,
-		    	0.1,
-		    	70
-		  	]
-		]
-	},
-	ridership_growth_5r: {
-		color: [
-			'interpolate',
-	    	['linear'],
-	    	['get', 'wkday_1yr_'],
-	    	-0.75,
-	    	'#ff6e6e',
-	    	0,
-	    	'#ff5252',
-	    	0.5,
-	    	'#ff2a2a'
-		],
-		radius: [
-			'interpolate',
-		  	['exponential', 1.75],
-		  	['zoom'],
-		  	11,
-		  	[
-		    	'interpolate',
-		    	['linear'],
-		    	['get', 'wkday_16'],
-		    	-0.75,
-		    	1,
-		    	0,
-		    	4,
-		    	0.5,
-		    	7
-		  	],
-		  	14,
-		  	[
-		    	'interpolate',
-		    	['linear'],
-		    	['get', 'wkday_16'],
-		    	-0.75,
-		    	10,
-		    	0,
-		    	15,
-		    	0.5,
-		    	30
-		  	],
-		  	16,
-		  	[
-		    	'interpolate',
-		    	['linear'],
-		    	['get', 'wkday_16'],
-		    	-0.75,
-		    	20,
-		    	0,
-		    	30,
-		    	0.5,
-		    	70
-		  	]
-		]
-	}
-};
-
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm94eWkiLCJhIjoiY2lrcThzbDZuMDA2eHVhbTdtd242OGMwaiJ9.59dG5F-n8Sp3_YjZfgeQRw';
 
 var map = new mapboxgl.Map({
@@ -172,51 +7,9 @@ var map = new mapboxgl.Map({
 	zoom: 12
 });
 
-map.on('load', function() {
-	map.addLayer({
-		id: 'ridership',
-		type: 'circle',
-		source: {
-			type: 'vector',
-			url: 'mapbox://roxyi.7q09tsrf'
-		},
-		'source-layer': 'subway_ridership',
-		paint: {
-			'circle-color': layerStyle.ridership_16.color,
-			'circle-radius': layerStyle.ridership_16.radius,
-  			'circle-opacity': 0.7
-		}
-	});
+var daySelected = 'weekday';
 
-	// map.on('click', 'ridership', function(e) {
-	// 	var feature = e.features[0];
-	// 	var diff = ((feature.properties.wkday_16 - 13404) * 100 / 13404).toFixed(1);
-	// 	var wkday_1yr_growth = (feature.properties.wkday_1yr_ * 100).toFixed(1);
-	// 	var wkday_5yr_growth = (feature.properties.wkday_5yr_ * 100).toFixed(1);
-	// 	var wknd_1yr_growth = (feature.properties.wknd_5yr_a * 100).toFixed(1);
-	// 	var wknd_5yr_growth = (feature.properties.wknd_5yr_a * 100).toFixed(1);
-	// 	new mapboxgl.Popup().setLngLat([feature.geometry.coordinates[0], feature.geometry.coordinates[1]])
-	// 	.setHTML(`	<p class="popup_title">${feature.properties.Station_Na} | ${controller.getTime()}</p>
-	// 				<p class="popup_text">Lines: ${feature.properties.lines}</p>
-	// 				<p class="popup_text">Avg. Daily Subway Ridership: <strong>${feature.properties.wkday_16.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</strong></p>
-	// 				<p class="popup_text"><span style="color: ${diff >=0 ? '#1fa543' : '#ff1f1f'}"><strong>${diff}%</strong></span> ${diff >= 0 ? 'Above' : 'Below'} Average</p>
- //                  	<p class="popup_text">Rank: <strong>${feature.properties.wkday_rank}</strong></p>
- //                  	<p class="marker_text_line" style="font-weight:bold;border-top: 1px solid #ddd; padding-top:10px;">In 2016</p>
- //                  	<p class="popup_text"></p>
- //                  	<p class="popup_text" style="font-weight:bold;">Last 5 years</p>
- //                  	<p class="popup_text"></p>
-	// 		`)
-	// 	.addTo(map);
-	// });
-
-	map.on('mouseenter', 'ridership', function() {
-		map.getCanvas().style.cursor = 'pointer';
-	});
-
-	map.on('mouseleave', 'ridership', function() {
-		map.getCanvas().style.cursor = '';
-	});
-});
+var popup = new mapboxgl.Popup();
 
 var layerSelectorModel = {
 	currentType: null,
@@ -303,6 +96,7 @@ var controller = {
 	init: function() {
 		layerSelectorModel.currentType = 'ridership2016';
 		layerSelectorView.render();
+		map.on('load', mapView.init);
 	},
 
 	getCurrentTypeData: function() {
@@ -336,11 +130,57 @@ var layerSelectorView = {
 	}
 };
 
+var mapView = {
+	init: function() {
+		map.addLayer({
+			id: 'ridership',
+			type: 'circle',
+			source: {
+				type: 'vector',
+				url: 'mapbox://roxyi.7q09tsrf'
+			},
+			'source-layer': 'subway_ridership',
+			paint: {
+	  			'circle-opacity': 0.7
+			}
+		});
+
+		controller.updateMap('wkday_16', [300, 20000, 70000]);
+
+		map.on('click', 'ridership', function(e) {
+			var feature = e.features[0];
+			var ridership = daySelected === 'weekday' ? feature.properties.wkday_16 : feature.properties.wknd_16;
+			var day = daySelected === 'weekday' ? 'Weekday' : 'Weekend';
+			var one_yr_growth = daySelected === 'weekday' ? parseInt(feature.properties.wkday_1yr_ * 100) : parseInt(feature.properties.wknd_1yr_a * 100);
+			var five_yr_growth = daySelected === 'weekday' ? parseInt(feature.properties.wkday_5yr_ * 100) : parseInt(feature.properties.wknd_1yr_a * 100);
+			popup.setLngLat([feature.geometry.coordinates[0], feature.geometry.coordinates[1]])
+			.setHTML(`	<p class="popup_title">${feature.properties.Station_Na} | ${day}</p>
+						<p class="popup_text">Lines: ${feature.properties.lines}</p>
+						<p class="popup_text">Avg. Daily Subway Ridership: <strong>${ridership.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</strong></p>
+	                  	<p class="popup_text">Rank: <strong>${feature.properties.wkday_rank}</strong></p>
+	                  	<p class="popup_text">In 2016 growth: <strong>${one_yr_growth}%</strong></p>
+	                  	<p class="popup_text">Last 5 years growth: <strong>${five_yr_growth}%</strong></p>
+				`)
+			.addTo(map);
+		});
+
+		map.on('mouseenter', 'ridership', function() {
+			map.getCanvas().style.cursor = 'pointer';
+		});
+
+		map.on('mouseleave', 'ridership', function() {
+			map.getCanvas().style.cursor = '';
+		});
+	}
+}
+
 document.querySelector('.radio-group').addEventListener('change', function(e) {
 	var selected1 = document.querySelector(`input[name='selector1']:checked`).value;
 	var selected2 = document.querySelector(`input[name='selector2']:checked`).value;
 	controller.updateMap(mapModel[selected1][selected2], layerSelectorModel.ramp[selected2]);
+	daySelected = selected1;
 	controller.updateSelector(selected2);
+	popup.remove();
 });
 
 controller.init();
